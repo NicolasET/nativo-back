fdfee4a4f4f139d29a66707e4ecd3bcdeaa2523a
NicolasET <nicolaset98@gmail.com>
2022-11-13 19:37:02 -0500

    ended

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Nicolas:nicolas@learning.pkmqs.mongodb.net/nativo',
+    ),
+    TaskModule,
+  ],
 })
 export class AppModule {}
